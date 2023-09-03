154d0dec70ae5d6d49977b57643c48d9f9f0790d
Tymur Bondar <tymurbondar@outlook.com>
2023-09-03 12:48:08 -0400

    add and delete tasks js

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    const taskInput = document.getElementById("task-input");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const taskList = document.getElementById("tasks");
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task");
+    taskItem.innerHTML = `
+        <label>${taskText}</label>
+        <button class="complete-button" onclick="completeTask(this)">Delete</button>
+    `; 
+
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+}
+
+function completeTask(button) {
+    const taskList = document.getElementById("tasks");
+    const taskItem = button.parentElement;
+    taskList.removeChild(taskItem);
+}
